7d442c3f5a2ee0cf088e72361c0328008df76c2b
eugeniucarvalho <eugeniucarvalho@hotmail.com>
2020-07-19 10:18:30 -0300

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
-		fmt.Fprint(w, "Hello! Your request was processed. 21")
+		fmt.Fprint(w, "Hello! Your request was processed. 2-1")
 	},
 	)
 	http.ListenAndServe(":8000", nil)
